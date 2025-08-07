11d0aaff9c8c9d7cb18ef5d4cee89e368fb0cef2
Naman20101 <namanreddy24@gmail.com>
2025-08-07 11:34:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function simulateTransaction(appName) {
   alert("Initiating fraud detection on " + appName + " transaction...");
   // You can trigger an API call here later if needed
 }
+function filterApps() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const icons = document.querySelectorAll("#paymentOptions a");
+
+  icons.forEach(icon => {
+    const name = icon.getAttribute("data-name");
+    if (name.includes(input)) {
+      icon.style.display = "inline-block";
+    } else {
+      icon.style.display = "none";
+    }
+  });
+}
