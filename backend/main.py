b423dc597fb7b433aae3dd8eaa7f57ea9fddc7ff
Naman20101 <namanreddy24@gmail.com>
2025-08-08 14:00:27 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,12 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 from url_checks import detect_typosquatting
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
